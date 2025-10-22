29bbaf028e7d30caee2c355c4b628908fa754784
nihaalkuchnagi <pes1202100537@pesu.pes.edu>
2025-10-22 12:55:14 +0530

    hash-object: Save object

--- ugit/cli.py
+++ ugit/cli.py
@@ -16,10 +16,18 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
     
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+    
     return parser.parse_args()
 
 #Command handler for 'init' command
 def init(args):
     data.init()
     print(f"Initialized empty ugit repository in {os.path.abspath(data.GIT_DIR)}")
+    
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(f"Hash of the file content: {data.hash_object(f.read())}")
     
--- ugit/data.py
+++ ugit/data.py
@@ -1,5 +1,14 @@
 import os
+import hashlib
+
 GIT_DIR = '.ugit'
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(os.path.join(GIT_DIR, 'objects'))
+    
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(GIT_DIR, 'objects', oid), 'wb') as out:
+        out.write(data)
+    return oid
