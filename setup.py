7fa034878b45cf23461716e672a457c9fd433497
nihaalkuchnagi <pes1202100537@pesu.pes.edu>
2025-10-22 12:00:21 +0530

    cli: Add argument parser

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
- 
+
 setup (name = 'ugit',
         version = '1.0',
         packages = ['ugit'],
         entry_points = {
             'console_scripts' : [
-                'ugit = ugit.cli:main'
+            'ugit = ugit.cli:main'
             ]
         })
