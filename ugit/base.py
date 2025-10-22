f0953ac0a7af25c5b0943da58465383d96ec91a7
nihaalkuchnagi <pes1202100537@pesu.pes.edu>
2025-10-22 13:47:17 +0530

    write-tree: Ignore .ugit files

--- ugit/base.py
+++ ugit/base.py
@@ -1,1 +1,27 @@
+import os
+from pathlib import Path
+
 import ugit.data as data
+
+def write_tree(directory='.'):
+    with os.scandir(directory) as it:
+        for entry in it:
+            full = os.path.join(directory, entry.name)
+            if is_ignored(full):
+                continue
+            if entry.is_file(follow_symlinks=False):
+                print(f"File: {full}")
+            elif entry.is_dir(follow_symlinks=False):
+                write_tree(full)
+
+def is_ignored(path):
+    if '.ugit' in Path(path).parts:
+        return True
+    elif '.git' in Path(path).parts:
+        return True
+    elif '__pycache__' in Path(path).parts:
+        return True
+    elif 'ugit.egg-info' in Path(path).parts:
+        return True
+    else:
+        return False
